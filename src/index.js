1d7a7522ea699a4e4f9f1d9a1ccd0fd430d154b0
Bryanhurtado0006 <bryandreshurtado18@gmail.com>
2025-07-08 17:29:48 -0500

    resolviendo

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ app.use(express.json());
 
 app.use("/api/usuarios", usuariosRoutes);
 app.use("/api/instituciones", institucionesRoutes);
+app.use("/api/tipos-de-prueba", tipoDePruebaRoutes);
 
 app.use((req, res) => {
   res.status(404).json({ mensaje: "Ruta no encontrada" });
